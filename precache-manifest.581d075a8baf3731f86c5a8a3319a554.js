568ef0e9e825eeaa221a7966d30b2352f556e9ad
Harisu Fanyui <fanyuiharisu@gmail.com>
2019-11-18 15:11:54 +0100

    Updates

---
+++ precache-manifest.581d075a8baf3731f86c5a8a3319a554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923f200e0f81220751d6161940a65ebf",
+    "url": "/me4real/index.html"
+  },
+  {
+    "revision": "c122c8d572790dc69534",
+    "url": "/me4real/static/css/2.fa56de13.chunk.css"
+  },
+  {
+    "revision": "43e8ea5df0d5bc1ead1b",
+    "url": "/me4real/static/css/main.3d89b319.chunk.css"
+  },
+  {
+    "revision": "c122c8d572790dc69534",
+    "url": "/me4real/static/js/2.9e3ae6cc.chunk.js"
+  },
+  {
+    "revision": "43e8ea5df0d5bc1ead1b",
+    "url": "/me4real/static/js/main.37f77b9f.chunk.js"
+  },
+  {
+    "revision": "b5f41fe0f574d6eaa7c5",
+    "url": "/me4real/static/js/runtime-main.8093fefa.js"
+  }
+]);
